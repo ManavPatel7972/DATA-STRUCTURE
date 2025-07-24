5d647fda64b10c1eaf26d2d9c9d68e421aed19ee
Manav Delvadiya <24010101056@darshan.ac.in>
2025-07-24 21:05:33 +0530

    Add files via upload

---
+++ PracticeQuestion/Tree/BST/BST_Iterator.java
@@ -0,0 +1,112 @@
+package BST;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BST_Iterator {
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode root = null;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void createTree() {
+        System.out.println("Enter a Root Node : ");
+        int val = sc.nextInt();
+        root = new TreeNode(val);
+        createTree(sc, root);
+    }
+
+    private static void createTree(Scanner sc, TreeNode node) {
+        System.out.println("left of " + node.val + " (Yes -> 1)(No -> 0)");
+        int left = sc.nextInt();
+
+        if (left == 1) {
+            System.out.println("Enter the value of the left of " + node.val);
+            int val = sc.nextInt();
+            node.left = new TreeNode(val);
+            createTree(sc, node.left);
+        }
+
+        System.out.println("right of " + node.val + " (Yes -> 1)(No -> 0)");
+        int right = sc.nextInt();
+        if (right == 1) {
+            System.out.println("Enter the value of the right of " + node.val);
+            int val = sc.nextInt();
+            node.right = new TreeNode(val);
+            createTree(sc, node.right);
+        }
+    }
+
+    public static void display(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void displayPretty(TreeNode node, int level) {
+
+        if (node == null) {
+            return;
+        }
+        displayPretty(node.right, level + 1);
+
+        if (level != 0) {
+
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t");
+            }
+            System.out.println("|------->" + node.val);
+        } else {
+            System.out.println(node.val);
+        }
+        displayPretty(node.left, level + 1);
+    }
+
+    class BSTIterator {
+
+        public BSTIterator(TreeNode root) {
+            inorder(root);
+        }
+        int i=0;
+        public int next() {
+            return li.get(i++);
+        }
+
+        public boolean hasNext() {
+            if(i > li.size()) return false;
+            return true;
+        }
+
+        List<Integer> li = new ArrayList<>();
+        public void inorder(TreeNode root){
+            if(root == null) return;
+            inorder(root.left);
+            li.add(root.val);
+            inorder(root.right);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        createTree();
+        System.out.println();
+        displayPretty(root, 1);
+    }
+
+}
+
+
