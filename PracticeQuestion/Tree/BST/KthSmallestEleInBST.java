0be8e8d96ce71ad9980883176793c6a1b05f2a02
Manav Delvadiya <24010101056@darshan.ac.in>
2025-07-24 21:03:33 +0530

    Add files via upload

---
+++ PracticeQuestion/Tree/BST/KthSmallestEleInBST.java
@@ -0,0 +1,100 @@
+package BST;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class KthSmallestEleInBST {
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode root = null;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void createTree() {
+        System.out.println("Enter a Root Node : ");
+        int val = sc.nextInt();
+        root = new TreeNode(val);
+        createTree(sc, root);
+    }
+
+    private static void createTree(Scanner sc, TreeNode node) {
+        System.out.println("left of " + node.val + " (Yes -> 1)(No -> 0)");
+        int left = sc.nextInt();
+
+        if (left == 1) {
+            System.out.println("Enter the value of the left of " + node.val);
+            int val = sc.nextInt();
+            node.left = new TreeNode(val);
+            createTree(sc, node.left);
+        }
+
+        System.out.println("right of " + node.val + " (Yes -> 1)(No -> 0)");
+        int right = sc.nextInt();
+        if (right == 1) {
+            System.out.println("Enter the value of the right of " + node.val);
+            int val = sc.nextInt();
+            node.right = new TreeNode(val);
+            createTree(sc, node.right);
+        }
+    }
+
+    public static void display(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void displayPretty(TreeNode node, int level) {
+
+        if (node == null) {
+            return;
+        }
+        displayPretty(node.right, level + 1);
+
+        if (level != 0) {
+
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t");
+            }
+            System.out.println("|------->" + node.val);
+        } else {
+            System.out.println(node.val);
+        }
+        displayPretty(node.left, level + 1);
+    }
+
+    public void addEle(TreeNode root,List<Integer> li ){
+        if(root == null) return;
+        addEle(root.left,li);
+        li.add(root.val);
+        addEle(root.right,li);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null) return -1;
+        List<Integer> li = new ArrayList<>();
+        addEle(root,li);
+        return li.get(k-1);
+    }
+
+    public static void main(String[] args) {
+        createTree();
+        System.out.println();
+        displayPretty(root, 1);
+    }
+
+}
+
+
+
