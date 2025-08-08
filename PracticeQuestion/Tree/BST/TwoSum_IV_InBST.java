86ead5838064d56c329154ebd99d38338678fe41
Manav Delvadiya <24010101056@darshan.ac.in>
2025-08-08 23:18:13 +0530

    Add files via upload

---
+++ PracticeQuestion/Tree/BST/TwoSum_IV_InBST.java
@@ -0,0 +1,107 @@
+package BST;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TwoSum_IV_InBST {
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode root = null;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void createTree() {
+        System.out.println("Enter a Root Node : ");
+        int val = sc.nextInt();
+        root = new TreeNode(val);
+        createTree(sc, root);
+    }
+
+    private static void createTree(Scanner sc, TreeNode node) {
+        System.out.println("left of " + node.val + " (Yes -> 1)(No -> 0)");
+        int left = sc.nextInt();
+
+        if (left == 1) {
+            System.out.println("Enter the value of the left of " + node.val);
+            int val = sc.nextInt();
+            node.left = new TreeNode(val);
+            createTree(sc, node.left);
+        }
+
+        System.out.println("right of " + node.val + " (Yes -> 1)(No -> 0)");
+        int right = sc.nextInt();
+        if (right == 1) {
+            System.out.println("Enter the value of the right of " + node.val);
+            int val = sc.nextInt();
+            node.right = new TreeNode(val);
+            createTree(sc, node.right);
+        }
+    }
+
+    public static void display(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void displayPretty(TreeNode node, int level) {
+
+        if (node == null) {
+            return;
+        }
+        displayPretty(node.right, level + 1);
+
+        if (level != 0) {
+
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t");
+            }
+            System.out.println("|------->" + node.val);
+        } else {
+            System.out.println(node.val);
+        }
+        displayPretty(node.left, level + 1);
+    }
+    public void inorder(TreeNode root , List<Integer> li){
+        if(root == null) return;
+        inorder(root.left,li);
+        li.add(root.val);
+        inorder(root.right,li);
+    }
+    public boolean helper(List<Integer> li,int k){
+       for(int i=0;i<li.size();i++){
+           for(int j = i+1 ; j< li.size() ; j++){
+               if(li.get(i) + li.get(j) == k){
+                   return true;
+               }
+           }
+       }
+       return false;
+    }
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> li = new ArrayList<>();
+        inorder(root,li);
+        return helper(li,k);
+    }
+
+    public static void main(String[] args) {
+        createTree();
+        System.out.println();
+        displayPretty(root, 1);
+    }
+
+}
+
+
