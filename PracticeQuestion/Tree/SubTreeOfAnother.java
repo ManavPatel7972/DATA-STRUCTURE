2a0332f925b27eab321e8f717a0ec12b187cfa25
Manav Delvadiya <24010101056@darshan.ac.in>
2025-08-08 23:20:48 +0530

    Add files via upload

---
+++ PracticeQuestion/Tree/SubTreeOfAnother.java
@@ -0,0 +1,108 @@
+
+package BT;
+
+import com.sun.source.tree.Tree;
+
+import java.util.Scanner;
+
+public class SubTreeOfAnother {
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+
+    static TreeNode root = null;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void createTree(){
+        System.out.println("Enter a Root TreeNode : ");
+        int val = sc.nextInt();
+        root = new TreeNode(val);
+        createTree(sc,root);
+    }
+
+    private static void createTree(Scanner sc,TreeNode node){
+        System.out.println("left of " + node.val + " (Yes -> 1)(No -> 0)");
+        int left = sc.nextInt();
+
+        if(left==1){
+            System.out.println("Enter the value of the left of " + node.val);
+            int val = sc.nextInt();
+            node.left = new TreeNode(val);
+            createTree(sc,node.left);
+        }
+
+        System.out.println("right of " + node.val + " (Yes -> 1)(No -> 0)");
+        int right = sc.nextInt();
+        if(right==1){
+            System.out.println("Enter the value of the right of " + node.val);
+            int val = sc.nextInt();
+            node.right = new TreeNode(val);
+            createTree(sc,node.right);
+        }
+    }
+
+    public static void display(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.val + " ");
+        display(node.left);
+        display(node.right);
+
+    }
+    public static void displayPretty(TreeNode node,int level){
+
+        if(node == null){
+            return;
+        }
+        displayPretty(node.right,level+1);
+
+        if(level!=0){
+            for(int i=0;i<level-1;i++){
+                System.out.print("|\t");
+            }
+            System.out.println("|------->" + node.val);
+        }
+        else{
+            System.out.println(node.val);
+        }
+        displayPretty(node.left,level+1);
+    }
+
+    public static boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+    public static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public static void main(String[] args) {
+        createTree();
+        System.out.println();
+        display(root);
+        System.out.println();
+    }
+
+}
+
+
+
+
+
+
