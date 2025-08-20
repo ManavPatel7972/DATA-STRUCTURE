0a071fc423ed5c8ce400a7750eb7112e3111d5ca
Manav Delvadiya <24010101056@darshan.ac.in>
2025-08-20 11:49:19 +0530

    Add files via upload

---
+++ PracticeQuestion/Tree/ConstructTreeUsingPre_In.java
@@ -0,0 +1,112 @@
+package BT;
+
+import java.util.Scanner;
+
+public class ConstructTreeUsingPre_In {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static TreeNode root = null;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void createTree() {
+        System.out.println("Enter a Root Node : ");
+        int val = sc.nextInt();
+        root = new TreeNode(val);
+        createTree(sc, root);
+    }
+
+    private static void createTree(Scanner sc, TreeNode node) {
+        System.out.println("left of " + node.val + " (Yes -> 1)(No -> 0)");
+        int left = sc.nextInt();
+
+        if (left == 1) {
+            System.out.println("Enter the value of the left of " + node.val);
+            int val = sc.nextInt();
+            node.left = new TreeNode(val);
+            createTree(sc, node.left);
+        }
+
+        System.out.println("right of " + node.val + " (Yes -> 1)(No -> 0)");
+        int right = sc.nextInt();
+        if (right == 1) {
+            System.out.println("Enter the value of the right of " + node.val);
+            int val = sc.nextInt();
+            node.right = new TreeNode(val);
+            createTree(sc, node.right);
+        }
+    }
+
+    public static void display(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        display(node.left);
+        display(node.right);
+
+    }
+
+    public static void displayPretty(TreeNode node, int level) {
+
+        if (node == null) {
+            return;
+        }
+        displayPretty(node.right, level + 1);
+
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->" + node.val);
+        } else {
+            System.out.println(node.val);
+        }
+        displayPretty(node.left, level + 1);
+    }
+
+    public static TreeNode helper(int[] pre,int ps,int pe,int[] in,int is,int ie){
+        if(ps>pe) return null;
+
+        TreeNode root = new TreeNode(pre[ps]);
+        int i = is;
+
+        while (in[i] != pre[ps]){
+            i++;
+        }
+        int ls = i-is;
+
+        root.left = helper(pre,ps+1,ps+ls,in,is,i-1);
+        root.right = helper(pre,ps+ls+1,pe,in,i+1,ie);
+        return root;
+    }
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return helper(preorder,0,n-1,inorder,0,n-1);
+    }
+
+    public static void main(String[] args) {
+//        createTree();
+        int[] preorder = {1,2,4,5,3,6};
+        int[] inorder = {4,2,5,1,3,6};
+        root = buildTree(preorder,inorder);
+        displayPretty(root, 0);
+    }
+}
